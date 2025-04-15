386472ff5c5c54fcbe5909ff14a668fdcb13c758
andresmotta9 <46831406+andresmotta9@users.noreply.github.com>
2025-04-14 20:14:42 -0400

    Added form state to register view

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -2,11 +2,26 @@ import { Link as RouterLink } from 'react-router-dom';
 import { AuthLayout } from '../layout/AuthLayout';
 import { Button, Grid, TextField, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks';
+
+const formData = {
+  email: 'email@email.com',
+  password: '123456',
+  displayName: 'Full Name Here',
+};
 
 export const RegisterPage = () => {
+  const { displayName, email, password, onInputChange, formState } =
+    useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  };
+
   return (
     <AuthLayout title="Login">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item size={12} sx={{ mt: 2 }}>
             <TextField
@@ -14,6 +29,9 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Full name"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -23,6 +41,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="email@mail.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -32,18 +53,21 @@ export const RegisterPage = () => {
               type="password"
               placeholder="password"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 2 }} size={12}>
             <Grid item size={12}>
-              <Button variant="contained" fullWidth>
+              <Button variant="contained" fullWidth type="submit">
                 Create Account
               </Button>
             </Grid>
           </Grid>
           <Grid container direction="row" justifyContent="end" size={12}>
-            <Typography sx={{ mr: 1 }}>Already has an account?</Typography>
+            <Typography sx={{ mr: 1 }}>Already have an account?</Typography>
             <Link component={RouterLink} color="inherit" to="/auth/login">
               Login
             </Link>
